67e48ac258612713cbf49af7b900682f166f38e0
Comicly69 <snowyoumc@gmail.com>
2023-04-22 20:20:01 -0400

    add ver 0.0.1

--- src/main/kotlin/window.kt
+++ src/main/kotlin/window.kt
@@ -1,12 +1,20 @@
+import java.awt.BorderLayout
+import java.awt.Font
+import javax.swing.ImageIcon
 import javax.swing.JFrame
 import javax.swing.JLabel
 
 fun main() {
-    val frame = JFrame("Hello World")
-    val label = JLabel("Hello, World!")
-    label.horizontalAlignment = JLabel.CENTER
-    label.verticalAlignment = JLabel.CENTER
-    frame.add(label)
+    val frame = JFrame("Cheese")
+    val imageIcon = ImageIcon("/home/max/code/lenny/src/assets/cheese.jpg")
+    val backgroundLabel = JLabel(imageIcon)
+    backgroundLabel.layout = BorderLayout()
+    val textLabel = JLabel("Cheese")
+    textLabel.font = Font("Arial", Font.BOLD, 36)
+    textLabel.horizontalAlignment = JLabel.CENTER
+    textLabel.verticalAlignment = JLabel.CENTER
+    backgroundLabel.add(textLabel)
+    frame.contentPane = backgroundLabel
     frame.setSize(400, 300)
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     frame.setLocationRelativeTo(null)
